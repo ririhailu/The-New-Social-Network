125252c0f5f7bf9d068b8da248a22ad3db3c2191
Matrix <rhailu88@gmail.com>
2023-01-13 12:45:20 -0500

    Add get singler user by id and return if no user in controllers file.

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -26,3 +26,23 @@ const usersController = {
         res.status(500).json(err);
     });
 },
+
+// Get single user by ID
+getUsersById({params}, res) {
+    Users.findOne({_id: params.id })
+    .populate({path: 'thoughts', select: '-__v'})
+    .populate({path: 'friends', select: '-__v'})
+    .select('-__v')
+    // return if no user is found 
+    .then(dbUsersData => {
+        if(!dbUsersData) {
+            res.status(404).json({message: 'No User with this particular ID!'});
+            return; 
+        }
+        res.json(dbUsersData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err)
+    })
+},
