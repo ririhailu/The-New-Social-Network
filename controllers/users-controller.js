8d4d928d6ef2410fc28d30986230b2c8b3f9e413
Matrix <rhailu88@gmail.com>
2023-01-13 12:42:26 -0500

    Created get all users in controllers file.

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -10,3 +10,19 @@ const usersController = {
     .then(dbUsersData => res.json(dbUsersData))
     .catch(err => res.status(400).json(err));
 },
+
+ // Get All Users
+ getAllUsers(req, res) {
+    Users.find({})
+    // populate users thoughts
+    .populate({path: 'thoughts', select: '-__v'})
+    // populate user friends
+    .populate({path: 'friends', select: '-__v'})
+    .select('-__v')
+    // .sort({_id: -1})
+    .then(dbUsersData => res.json(dbUsersData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+},
