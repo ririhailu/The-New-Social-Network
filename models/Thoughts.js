e785e1e60b46f642d62f2414d8b47f71b32d51fc
Matrix <rhailu88@gmail.com>
2023-01-13 12:55:09 -0500

    Created reaction schema and custom id in moddels file.

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,3 +1,33 @@
 // Require Mongoos and Moment
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
+
+// ReactionsSchema
+const ReactionsSchema = new Schema(
+    {
+    // Set custom ID 
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+    },
+    {
+    toJSON: {
+        getters: true
+    } 
+    }
+);
