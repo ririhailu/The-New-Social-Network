3a6a73e749659ee8537279e3b797aca9640ff3a3
Matrix <rhailu88@gmail.com>
2023-01-13 11:29:12 -0500

    Create new thought in controllers file.

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -3,3 +3,19 @@ const {Thoughts, Users} = require('../models');
 
 // Set up Thoughts Controller
 const thoughtsController = {
+
+    // Create a new thought
+    createThoughts({params, body}, res) {
+        Thoughts.create(body)
+        .then(({_id}) => {
+            return Users.findOneAndUpdate({ _id: params.userId}, {$push: {thoughts: _id}}, {new: true});
+        })
+        .then(dbThoughtsData => {
+            if(!dbThoughtsData) {
+                res.status(404).json({message: 'No thoughts with this particular ID!'});
+                return;
+            }
+            res.json(dbThoughtsData)
+        })
+        .catch(err => res.json(err)); 
+    },
