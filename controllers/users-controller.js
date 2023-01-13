7c063435a684c73a467e015e55947e274c6ff069
Matrix <rhailu88@gmail.com>
2023-01-13 12:41:23 -0500

    Create new user in controllers file.

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -3,3 +3,10 @@ const {Users} = require('../models');
 
 // Set up Users Controller
 const usersController = {
+
+ // Create a new User
+ createUsers({body}, res) {
+    Users.create(body)
+    .then(dbUsersData => res.json(dbUsersData))
+    .catch(err => res.status(400).json(err));
+},
